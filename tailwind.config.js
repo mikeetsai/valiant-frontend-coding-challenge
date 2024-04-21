d103cefdd81106d802e35fc7c492f8bfbf7db61b
Mike Tsai <mike.tsai@careerone.com.au>
2024-04-21 16:33:58 +1000

    feat: add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     './src/**/*.{vue,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: '#272c2d',
+        },
+        secondary: {
+          500: '#65dc74',
+        },
+        danger: {
+          500: '#f74d4d',
+        },
+        light: {
+          500: '#e7e9ec',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
