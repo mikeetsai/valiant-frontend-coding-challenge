c837adcb9fd668ad2dc37af1d6956b1c9ee8a651
Mike Tsai <mike.tsai@careerone.com.au>
2024-04-22 10:31:55 +1000

    update: cta layout

--- src/components/RepaymentCalculator/index.vue
+++ src/components/RepaymentCalculator/index.vue
@@ -17,28 +17,28 @@ const totalRepayment = computed(() => addCommasToNumber(getResult.value.totalRep
     <div class="mx-auto mb-10 w-full max-w-2xl">
       <RCForm v-model:calculate="getResult" />
     </div>
-    <div
-      v-if="getResult.monthlyRepayment"
-      class="result bg-light-500 px-6 py-10 text-center shadow-sm"
-    >
-      <h2 class="mb-6 text-4xl font-bold text-secondary-600">
-        ${{ monthlyRepayment }} Monthly repayments
-      </h2>
 
-      <h2 class="text-3xl font-bold text-primary-500">
-        ${{ totalRepayment }} Total repayments
-      </h2>
-    </div>
-    <div class="flex items-center justify-center py-4 text-center text-primary-500">
-      <span>Do you need a quote?</span>
-      <a
-        href="https://app.valiant.finance/quote/start"
-        class="ml-2 font-semibold text-primary-500 underline hover:text-secondary-600"
-        target="_blank"
-        rel="nofollow"
-      >
-        Start here
-      </a>
-    </div>
+    <template v-if="getResult.monthlyRepayment">
+      <div class="result bg-light-500 px-6 py-10 text-center shadow-sm">
+        <h2 class="mb-6 text-4xl font-bold text-secondary-600">
+          ${{ monthlyRepayment }} Monthly repayments
+        </h2>
+
+        <h2 class="text-3xl font-bold text-primary-500">
+          ${{ totalRepayment }} Total repayments
+        </h2>
+      </div>
+      <div class="flex items-center justify-center py-4 text-center text-primary-500">
+        <span>Do you need a quote?</span>
+        <a
+          href="https://app.valiant.finance/quote/start"
+          class="ml-2 font-semibold text-primary-500 underline hover:text-secondary-600"
+          target="_blank"
+          rel="nofollow"
+        >
+          Start here
+        </a>
+      </div>
+    </template>
   </div>
 </template>
